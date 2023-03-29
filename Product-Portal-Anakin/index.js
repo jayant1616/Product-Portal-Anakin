81c3e432a74b15af76f8734be4866be996ab37da
Jayant <jay03102001@gmail.com>
2023-03-29 06:46:28 +0530

    change the env variable

--- Product-Portal-Anakin/index.js
+++ Product-Portal-Anakin/index.js
@@ -1,7 +1,7 @@
 const express  = require('express');
 const dotenv = require('dotenv');
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'production') {
   dotenv.config({ path: '.env.production' });
 } else { 
     dotenv.config({ path: '.env.development' });
